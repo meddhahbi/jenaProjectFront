b5f055854176d4234cf22bffb4a1b9c72829ddb8
Mootez <mootez@groopay.fr>
2023-10-31 15:03:51 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import MainInterface from './Component/MainInterface';
-import Home from './Component/pages/Home';
-import CategoriesList from './Component/pages/CategoriesList';
-=======
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import MainInterface from "./Component/MainInterface";
 import Home from "./Component/pages/Home";
+import CategoriesList from "./Component/pages/CategoriesList";
 import { Alert } from "./Component/pages/alert/Alert";
 import { ReplyAlert } from "./Component/pages/alert/ReplyAlert";
->>>>>>> mootez
 
 function App() {
   return (
@@ -19,8 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainInterface />}>
           <Route index element={<Home />} />
-          <Route path='/category' element={<CategoriesList />}/>
-
+          <Route path="/category" element={<CategoriesList />} />
           <Route path="/alert" element={<Alert />} />
           <Route path="/reply" element={<ReplyAlert />} />
         </Route>
