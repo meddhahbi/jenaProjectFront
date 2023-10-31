6fc58e7190ba93e52cc95a81660ce5855f374f4e
Mootez <mootez@groopay.fr>
2023-10-31 13:36:14 +0100

    alerts+reply

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import MainInterface from './Component/MainInterface';
-import Home from './Component/pages/Home';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainInterface from "./Component/MainInterface";
+import Home from "./Component/pages/Home";
+import { Alert } from "./Component/pages/alert/Alert";
+import { ReplyAlert } from "./Component/pages/alert/ReplyAlert";
 
 function App() {
   return (
-   <div>
-    <Routes>
-      <Route path='/' element={<MainInterface />}>
-
+    <div>
+      <Routes>
+        <Route path="/" element={<MainInterface />}>
           <Route index element={<Home />} />
 
-
-      </Route>
-    </Routes>
-   </div>
+          <Route path="/alert" element={<Alert />} />
+          <Route path="/reply" element={<ReplyAlert />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./alert/alert.css";
 const Home = () => {
   return (
     <div>
