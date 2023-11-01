8e0dc288f93e25f90db6aa958013abf14a6bec29
Eya Ouertatani <eya.ouertatani@labs.baycii.com>
2023-10-31 21:40:54 -0800

    Categ/projet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainInterface from './Component/MainInterface';
 import Home from './Component/pages/Home';
 import CategoriesList from './Component/pages/CategoriesList';
+import ProjectsList from './Component/pages/ProjectsList';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
           <Route index element={<Home />} />
           <Route path='/category' element={<CategoriesList />}/>
+          <Route path='/project' element={<ProjectsList />}/>
 
 
       </Route>
